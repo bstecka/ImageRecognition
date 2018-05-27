a20fe83edcf8374f05933d8e74e90b83f8423f7f
negacja <bstecka@gmail.com>
2018-05-28 00:40:40 +0200

    Rearrange methods

--- src/com/company/GUI/ImagesView.java
+++ src/com/company/GUI/ImagesView.java
@@ -8,7 +8,7 @@ import org.opencv.imgcodecs.Imgcodecs;
 
 public class ImagesView {
     Image image1, image2;
-    Mat mat1, mat2;
+    Mat mat1, mat2, mat3;
     KeyPoint[] keyPoints1, keyPoints2;
     DMatch[] matches;
 
@@ -29,20 +29,24 @@ public class ImagesView {
             keyPoints1[i] = new KeyPoint((float) image1.keyPoints[i].x, (float) image1.keyPoints[i].y, keyPointDiameter);
         for (int i = 0; i < image2.keyPoints.length; i++)
             keyPoints2[i] = new KeyPoint((float) image2.keyPoints[i].x, (float) image2.keyPoints[i].y, keyPointDiameter);
+        createMats();
+        matchPoints();
     }
 
-    public void createMats() {
+    public void show() {
+        HighGui.imshow("Image Recognition", mat3);
+        HighGui.waitKey(0);
+    }
+
+    private void createMats() {
         this.mat1 = Imgcodecs.imread(image1.imagePath);
         this.mat2 = Imgcodecs.imread(image2.imagePath);
     }
 
-    public void matchPoints() {
-        Mat output = new Mat();
+    private void matchPoints() {
         MatOfKeyPoint matKey1 = new MatOfKeyPoint(keyPoints1);
         MatOfKeyPoint matKey2 = new MatOfKeyPoint(keyPoints1);
         MatOfDMatch matDMatch = new MatOfDMatch(matches);
-        Features2d.drawMatches(mat1, matKey1, mat2, matKey2, matDMatch, output);
-        HighGui.imshow("Image Recognition", output);
-        HighGui.waitKey(0);
+        Features2d.drawMatches(mat1, matKey1, mat2, matKey2, matDMatch, mat3);
     }
 }
